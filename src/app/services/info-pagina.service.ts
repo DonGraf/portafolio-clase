c7934cefc0f99b177b00b8bb645b2081b1f42b24
DonGraf <f.perez.pincheira@gmail.com>
2022-08-28 15:44:27 -0400

    Agrego la base de datos con Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,13 +10,26 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada:boolean = false;
 
+  equipo: any =[];
+
   constructor( private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
-      this.info = resp;
+      this.info = resp; 
+      });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-5f10a-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp ) => {
+      this.equipo = resp;
       console.log(resp);    
       });
-
   }
 }
