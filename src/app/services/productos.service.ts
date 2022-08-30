5f0995acc90212680d6e0fdfd87d89bb7174db0e
DonGraf <f.perez.pincheira@gmail.com>
2022-08-29 22:56:11 -0400

    Buscador Agregado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productoFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
@@ -16,15 +17,48 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-5f10a-default-rtdb.firebaseio.com/productos_idx.json')
-        .subscribe( (resp: any) => {
-          this.productos = resp;
-          this.cargando = false;
-        });
+
+    return new Promise((resolve:any, reject) => {
+      this.http.get('https://angular-html-5f10a-default-rtdb.firebaseio.com/productos_idx.json')
+          .subscribe( (resp: any) => {
+            this.productos = resp;
+            this.cargando = false;
+            resolve();
+          });
+    });
   }
   
 
   getProducto( id: string){
     return this.http.get(`https://angular-html-5f10a-default-rtdb.firebaseio.com/productos/${id}.json`);
   }
+
+buscarProducto(termino: string){
+
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //ejecutar despues de tener los productos, aca se aplica el filtro
+        this.filtrarProducto(termino);
+      });
+    }else{
+      //aplicar el filtro
+      this.filtrarProducto(termino);
+    }
+
+  console.log(this.productoFiltrado);
+}
+
+private filtrarProducto( termino: string){
+  this.productoFiltrado = [];
+  termino = termino.toLocaleLowerCase();
+  this.productos.forEach(prod => {
+
+    const tituloLower = prod.titulo.toLocaleLowerCase();
+    if( prod.categoria.indexOf(termino)>= 0 || tituloLower.indexOf(termino)>=0){
+      this.productoFiltrado.push(prod);
+    }
+  });
+}
+
 }
